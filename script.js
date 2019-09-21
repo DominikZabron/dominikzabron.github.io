43ace559d1a47589df964026ce220ff4d7482984
= <=>
2019-09-21 02:16:17 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+var modal = document.getElementById("modal");
+
+var btn = document.getElementById("msg-btn");
+
+btn.onclick = function() {
+  if (modal.style.display === "") {
+    modal.style.display = "block";
+  } else if (modal.style.display == "block") {
+    modal.style.display = "";
+  } else {
+    console.log(modal.style.display)
+  };
+}
+
+window.onclick = function(event) {
+  if (event.target === modal) {
+    modal.style.display = "";
+  }
+}
